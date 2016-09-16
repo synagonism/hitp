0abb1f99e550f18e6545415bb10b605492c97b6a
Kaseluris.Nikos.1959 <synagonism@gmail.com>
2016-09-16 21:14:13 +0300

    14-20.2016-09-16.js-toc-width

--- hitp.js
+++ hitp.js
@@ -1,5 +1,5 @@
 /*
- * version.14-5.2016-08-06.last.minor: hitp.js
+ * version.14-6.2016-09-16.last.minor: hitp.js
  * version.14.2016-06-09.last.minorNo (13): hitp.14.2016-06-09.js (table-content-tree)
  * version.13.2016-06-07 (12-11): hitp.13.2016-06-07.js (preview)
  * version.12.2016-01-24 (11.9): hitp.2016.01.24.12.js (toc-icn-img)
@@ -69,7 +69,7 @@ var oHitp = (function () {
 
   var oHitp = {
     /* config */
-    nCfgPosSplitPrev: 272,
+    nCfgPosSplitPrev: parseInt(window.outerWidth * 0.25),
     /**
      * config: the hitmenu contains absolute urls, because we see it from many pages.
      * Then we must know the ROOT of the site and create different menus.
